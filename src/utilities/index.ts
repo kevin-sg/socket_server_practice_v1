6f9c69e78af2eb610f239746455547511418bd6a
Kevin SG <kevin.boster99@gmail.com>
2022-05-19 19:09:13 -0300

    response handler updated

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -2,4 +2,4 @@ export * from "./db-connect.utility";
 export * from "./cors-options.utility";
 export * from "./socket-options.utility";
 export * from "./socket-key-event.utility";
-export * from "./response_handler.utility";
+export * from "./response-handler.utility";
