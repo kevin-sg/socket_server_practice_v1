2111a686f90adb2daa97c601fdc3bd3ab2848dfa
Kevin SG <kevin.boster99@gmail.com>
2022-05-23 14:57:23 -0300

    Updated

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,5 +1,3 @@
-export * from "./db-connect.utility";
 export * from "./cors-options.utility";
 export * from "./socket-options.utility";
 export * from "./socket-key-event.utility";
-export * from "./response-handler.utility";
