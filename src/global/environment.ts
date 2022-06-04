4a9168eb5f663569abda7ff6a5084d45481d10cd
Kevin SG <kevin.boster99@gmail.com>
2022-06-04 18:53:45 -0300

    Server PORT updated

--- src/global/environment.ts
+++ src/global/environment.ts
@@ -1,7 +1,7 @@
 import type { IEnvironmentVariables } from "@/global/global-types";
 
 export const environmentVariables: IEnvironmentVariables = {
-  HOST_PORT: 80,
+  HOST_PORT: 8080,
   HOST_LOCAL: "http://localhost:3000",
   HOST_CLIENT: "https://socket-io-band.netlify.app",
   // HOST_PORT: parseInt(process.env.PORT as string),
